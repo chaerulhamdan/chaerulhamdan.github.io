7f046a29b39bee3907dabf7b3b17c593d3a81727
chaerulhamdan <chaerul.hamdan@binus.ac.id>
2024-12-03 15:30:17 +0700

    update cv

--- src/data/constants.js
+++ src/data/constants.js
@@ -9,7 +9,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/chaerulhamdan",
   resume:
-    "https://drive.google.com/file/d/1ffZrcMcn8UatXGIaautbbqpV7ADNaETA/view?usp=sharing",
+    "https://drive.google.com/file/d/1_74czz3jKbXXzDS0HPslDECWfV5N4z4B/view",
   linkedin: "https://www.linkedin.com/in/chaerul-hamdan/",
   insta: "https://www.instagram.com/chaerul_hamdan/",
 };
