00e0f11f803d59b38078183b775114522ed9d0ef
chaerulhamdan <chaerul.hamdan@binus.ac.id>
2024-12-03 12:50:27 +0700

    personal portfolio

---
+++ src/data/constants.js
@@ -0,0 +1,309 @@
+export const Bio = {
+  name: "Chaerul Hamdan",
+  roles: [
+    "Full Stack Developer",
+    "Android Developer",
+    "Programmer",
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/chaerulhamdan",
+  resume:
+    "https://drive.google.com/file/d/1ffZrcMcn8UatXGIaautbbqpV7ADNaETA/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/chaerul-hamdan/",
+  insta: "https://www.instagram.com/chaerul_hamdan/",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+      {
+        name:"Vue Js",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/1200px-Vue.js_Logo_2.svg.png"
+      },
+      {
+        name:"Vuetify",
+        image: "https://webdevnerdstuff.github.io/vuetify-date-field/vuetify-logo.svg"
+      },
+      {
+        name:"Nuxt Js",
+        image: "https://www.inkoop.io/static/3fd155c993893819981d0b9fa56b5057/b7804/logo.png"
+      },
+      
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node Js",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },
+      {
+        name: "Adonis",
+        image: "https://avatars.githubusercontent.com/u/13810373?s=280&v=4",
+      },
+      {
+        name: "Laravel",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Laravel.svg/1200px-Laravel.svg.png",
+      },
+      {
+        name: "Asp .NET",
+        image: "https://miro.medium.com/v2/resize:fit:696/1*nre5clYJmnFaOF_goXkVIQ.png",
+      },
+      {
+        name: "Sql Server",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUP51SgZ1Hr9DwHn21MpR1jICjTqPWKYlKmg&s",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      
+      
+      
+    ],
+  },
+  {
+    title: "Mobile",
+    skills: [
+      {
+        name:"React Native",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png"
+      },
+      {
+        name: "Android Studio",
+        image:
+          "https://developer.android.com/static/studio/images/new-studio-logo-1_1920.png",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://avatars.githubusercontent.com/u/18133?s=280&v=4",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "GitLab",
+        image:
+          "https://yt3.googleusercontent.com/R6P5skGdZJeM1bebvt3ILeU8k-9tiqE5T198RmBH8SoGXH2gk_Lk-45uZoq6X6pW4a4c9Sqn=s900-c-k-c0x00ffffff-no-rj",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://yt3.googleusercontent.com/_q52i8bUAEvcb7JR4e-eNTv23y2A_wg5sCz0NC0GrGtcw1CRMWJSOPVHUDh_bngD0q4gMvVeoA=s900-c-k-c0x00ffffff-no-rj",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://yt3.googleusercontent.com/X-rhKMndFm9hT9wIaJns1StBfGbFdLTkAROwm4UZ3n9ucrBky5CFIeeZhSszFXBgQjItzCD0SA=s900-c-k-c0x00ffffff-no-rj",
+      },
+      {
+        name: "Figma",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSoUX5LMRa7atIsNfl0nP3DaUaV4URhV0PHfA&s",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://karirlab-prod-bucket.s3.ap-southeast-1.amazonaws.com/files/privates/ilRsIWpFMbKKpNqGcsHCEQplZCkFsZxMAgpczPee.jpg",
+    role: "Programmer & Mobile Develover Intern",
+    company: "PT. Maybank Indonesia Finance",
+    date: "Feb 2023 - Feb 2024",
+    desc: `As a Programmer and Mobile Developer intern at Maybank Finance Indonesia, 
+    I worked on developing and improving the company's internal Mobile Approval application (MApproval) using React Native. 
+    I collaborated with cross-functional teams to implement new features, participated in Agile workflows using Jira, 
+    and ensured the application ran smoothly by identifying and resolving bugs. 
+    This experience provided valuable insights into mobile app development and teamwork in a professional setting.`,
+    skills: [
+      "React Native",
+      "Asp .NET",
+      "VueJs",
+      "Vuetify",
+      "SQL Server",
+      "Jira",
+    ],
+    doc: "https://res.cloudinary.com/dz1yqkmb4/image/upload/v1733068128/WhatsApp_Image_2024-08-31_at_09.24.58_756737c7_ooei19.jpg",
+  },
+  
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLh3SkJ_NF4h9wXDEjTwXW6QQRnMceRIveyg&s",
+    school: "Binus University",
+    date: "Sep 2020 - Jul 2024",
+    grade: "3.70/4.00 GPA",
+    desc: "I am a graduate of Binus University with a Bachelor's degree in Computer Science and a GPA of 3.70/4.00. During my studies, I gained expertise in courses such as Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks. This academic experience provided a solid foundation for understanding and developing information technology concepts.",
+    degree: "Bachelor's degree Computer Science",
+  },
+  {
+    id: 1,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYTRtvTRQKlvadEJxebfBQ7Lw34n7-1tY7nA&s",
+    school: "NodeJs Backend Development with Adonis (Bootcamp at Sanbercode)",
+    date: "Oct 2022 - Nov 2022",
+    grade: "96/100, rank 1 at leaderboard batch 39",
+    desc: "I Completed the program Bootcamp NodeJs Backend Development Adonis with a final project score of 96, and earning 1st rank on the leaderboard.",
+    degree: "Mastered NodeJS Backend Development with Adonis",
+  },
+  {
+    id: 2,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYTRtvTRQKlvadEJxebfBQ7Lw34n7-1tY7nA&s",
+    school: "Laravel - Web Development (Bootcamp at Sanbercode)",
+    date: "Oct 2022 - Nov 2022",
+    grade: "100/100, rank 1 at leaderboard batch 39",
+    desc: "I Completed the program Bootcamp Laravel - Web Development at Sanbercode with a perfect score (100) on the final project and achieved 1st rank on the leaderboard.",
+    degree: "Mastered Laravel Web Development",
+  },
+];
+
+export const projects = [
+  {
+    id: 9,
+    title: "Attendance System",
+    date: "Dec 2022 - Dec 2022",
+    description:
+      "This Attendance System Web is a Laravel-based platform designed to support organizational activities. In this project, I served as the team leader. The web provides features that allow members to log their attendance, view information about upcoming events, and share ideas or suggestions for planned events. The system is designed to enhance the efficiency of organizational activity management while encouraging active participation from members.",
+    image:
+      "https://res.cloudinary.com/dz1yqkmb4/image/upload/v1733111981/qzstytmyc0a8vgttqzel.png",
+    tags: [
+      "Laravel",
+      "PHP",
+      "MySql",
+    ],
+    category: "web app",
+    github: "https://github.com/chaerulhamdan/web-laravel",
+    // webapp: "https://trackify.duckdns.org",
+  },
+  {
+    id: 0,
+    title: "Backend Book Store",
+    date: "Apr 2023 - May 2023",
+    description:
+      "This project is developed as the final assignment for the Node.js Backend with Adonis.js bootcamp. The backend is designed to manage bookstore operations, including book inventory and rental transactions, simulating real-world bookstore functionalities.",
+    image:
+      "https://res.cloudinary.com/dz1yqkmb4/image/upload/v1733117353/yvfav0l8uzba5hthjhdf.png",
+    tags: ["Adonis Js", "Node Js", "MySql"],
+    category: "console app",
+    github: "https://gitlab.com/chaerulhamdan/project-final-adonis/-/tree/master",
+    // webapp: "https://podstream.netlify.app/",
+  },
+  {
+    id: 1,
+    title: "Backend Selaga Apps",
+    date: "Mar 2024 - Jul 2024",
+    description:
+      "Backend Selaga Apps is a backend service developed to support a sports venue rental application, specifically for badminton courts. This project was created as part of a thesis to facilitate efficient management of sports venue rentals.",
+    image:
+      "https://res.cloudinary.com/dz1yqkmb4/image/upload/v1733118767/qbyordirmidnu5zxwmxf.png",
+    tags: [
+      "Laravel",
+      "PHP",
+      "MySql",
+    ],
+    category: "console app",
+    github: "https://github.com/chaerulhamdan/Selaga-Backend-Laravel",
+    // webapp: "https://vexa-app.netlify.app/",
+  },
+  {
+    id: 2,
+    title: "Responsive and Animated Personal Portfolio",
+    date: "Aug 2023 - Sep 2023",
+    description:
+      "This project is a personal portfolio website designed with a responsive layout, ensuring optimal accessibility across various devices and screen sizes, from desktops to smartphones. The website features interactive animations that enhance visual appeal and provide an engaging user experience.",
+    image:
+      "https://res.cloudinary.com/dz1yqkmb4/image/upload/v1733119323/ryjs4ttzd9lpy3oiiiwm.png",
+    tags: ["HTML", "CSS", "JavaScript"],
+    category: "web app",
+    github: "https://github.com/chaerulhamdan/Responsive-Personal-Portfolio",
+    // webapp: "https://brain-tumor.netlify.app/",
+  },
+  {
+    id: 3,
+    title: "Console Restaurant using Java with Object Oriented Programming",
+    date: "Dec 2021 - Apr 2022",
+    description:
+      "Console Restaurant Apps Using Java This project is a console-based application developed using Java with Object-Oriented Programming (OOP) principles. It is designed to assist restaurants in managing menus, streamlining the ordering process, and recording purchased items efficiently.",
+    image:
+      "https://res.cloudinary.com/dz1yqkmb4/image/upload/v1733120516/gfyddmv5braejyft6xco.png",
+    tags: ["Java", "MySQL"],
+    category: "console app",
+    github: "https://github.com/chaerulhamdan/Project-Design-Pattern-OOP",
+    // webapp: "https://play.google.com/store/apps/details?id=com.rishav.buckoid",
+  },
+  {
+    id: 10,
+    title: "Selaga Apps",
+    date: "Mar 2024 - Jul 2024",
+    description:
+      "Selaga Apps is a mobile Android application developed using Flutter. It is designed to make renting badminton courts online quick and easy. This project was undertaken as part of a thesis.",
+    image:
+      "https://res.cloudinary.com/dz1yqkmb4/image/upload/v1733122674/ai6tiqf3gkpddimo6hto.png",
+    tags: ["Dart", "Flutter", "MySQL"],
+    category: "android app",
+    github: "https://github.com/chaerulhamdan/Selaga",
+    // webapp: "https://github.com/rishavchanda/Job-finder-App",
+    member: [
+      {
+        name: "Chaerul Hamdan",
+        img: "https://avatars.githubusercontent.com/u/76488608?s=400&u=93f094f0c1de0c4b270050591f53b099abd97447&v=4",
+        linkedin: "https://www.linkedin.com/in/chaerul-hamdan/",
+        github: "https://github.com/chaerulhamdan/",
+      },
+      {
+        name: "Brian Abbiyu Paras",
+        img: "https://avatars.githubusercontent.com/u/115937389?v=4",
+        linkedin: "https://www.linkedin.com/in/brian-abiyyu-farras-9a0927252/",
+        github: "https://github.com/brianabyyuf",
+      },
+    ],
+
+  },
+];
+
+export const TimeLineData = [
+  { year: 2017, text: "Started my journey" },
+  { year: 2018, text: "Worked as a freelance developer" },
+  { year: 2019, text: "Founded JavaScript Mastery" },
+  { year: 2020, text: "Shared my projects with the world" },
+  { year: 2021, text: "Started my own platform" },
+];
